c28b0c1527a57eaa5139779c3e88ea97b699dd53
FCWY <fcwyzzr@163.com>
2025-03-13 15:31:40 +0800

    fetch more task :(

---
+++ SDL_error.hpp
@@ -0,0 +1,13 @@
+//
+// Created by FCWY on 25-3-13.
+//
+
+#ifndef SDL_ERROR_HPP
+#define SDL_ERROR_HPP
+#include <SDL3/SDL_Error.h>
+
+namespace SDL::plus {
+    struct Error;
+}
+
+#endif //SDL_ERROR_HPP
---
+++ include/SDL3plus/SDL_endian.hpp
@@ -0,0 +1,9 @@
+//
+// Created by FCWY on 25-3-13.
+//
+
+#ifndef SDL_ENDIAN_HPP
+#define SDL_ENDIAN_HPP
+#include <SDL3/SDL_endian.h>
+
+#endif //SDL_ENDIAN_HPP
